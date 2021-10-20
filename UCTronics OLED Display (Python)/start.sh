94acea44a42659d6e849f93181f2180d65b2f59f
garethcheyne <gareth.cheyne@nz.harveynorman.com>
2021-10-20 16:55:07 +1300

    update

---
+++ UCTronics OLED Display (Python)/start.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bashio
+set -e
+
+CONFIG_PATH=/data/options.json
+
+TEMP_UNIT="$(bashio::config 'Temperature_Unit')"
+DISABLE_AUTO_START="$(bashio::config 'Stop_Auto_Run')"
+bashio::log.info "Starting UCTronics OLED App..."
+bashio::log.info "Disable Auto Start = ${DISABLE_AUTO_START}"
+
+if [ "$DISABLE_AUTO_START" = false ]; then
+    cd /UCTronics_OLED_Python/
+    python3 display.py
+
+fi
