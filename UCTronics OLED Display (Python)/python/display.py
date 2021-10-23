f0e6d0f4a2ecf08fb2adbe52c9c6b2ff2d1d2f41
gareth.cheyne <gareth.cheyne@nz.harveynorman.com>
2021-10-24 11:39:22 +1300

    update

--- UCTronics OLED Display (Python)/python/display.py
+++ UCTronics OLED Display (Python)/python/display.py
@@ -13,7 +13,6 @@ import time
 import sys, getopt
 import subprocess
 import json
-from gpiozero import OutputDevice
 
 from board import SCL, SDA
 import busio
@@ -252,9 +251,6 @@ def get_options():
     SHOW_NETWORK = options['Show_Network_Info']
     DURATION =  options['Slide_Duration']
 
-def fan_control():
-    fan = OutputDevice(2)
-    print(fan.value)
 
 if __name__ == "__main__":
     get_options()
